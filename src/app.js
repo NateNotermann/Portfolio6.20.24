717e17d019d6097a2996f032c2c3c971171d22b8
Nate Notermann <98718660+NateNotermann@users.noreply.github.com>
2024-08-04 19:56:49 -0500

    got GSAP to work with script tag. don need to import in the javascript file.

---
+++ src/app.js
@@ -0,0 +1,17 @@
+
+document.querySelector(".box").innerHTML = " BOO";
+
+
+document.addEventListener("DOMContentLoaded", (event) => {
+    gsap.to(".box", {
+        duration: 3, 
+        rotation: 360, 
+        scale: 2,
+        });
+        
+        })
+
+
+
+
+console.log('HELLO WOLRD');
