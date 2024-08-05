7027d0609f28829b8a5c777b197f7f94593b2bcc
Nate Notermann <98718660+NateNotermann@users.noreply.github.com>
2024-08-04 20:00:33 -0500

    more gsap tests

--- src/app.js
+++ src/app.js
@@ -4,9 +4,12 @@ document.querySelector(".box").innerHTML = " BOO";
 
 document.addEventListener("DOMContentLoaded", (event) => {
     gsap.to(".box", {
-        duration: 3, 
-        rotation: 360, 
-        scale: 2,
+        duration: 2, 
+        rotation: 720,     // Duration of the animation
+        y: 300,           // Move the box down by 300px
+        ease: "bounce.out", // Use the bounce easing function
+        repeat: 2,       // Repeat indefinitely
+        yoyo: true       
         });
         
         })
